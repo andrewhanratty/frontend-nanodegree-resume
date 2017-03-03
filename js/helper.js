f172d6e0de878a69cb11737139f8bcc82b7ff7cd
Andrew <drew.hanratty@gmail.com>
2017-03-03 10:35:18 -0800

    Add updates to helper and resumeBuilder JSONs

--- js/helper.js
+++ js/helper.js
@@ -12,8 +12,8 @@ Cameron Pittman
 These are HTML strings. As part of the course, you'll be using JavaScript functions
 replace the %data% placeholder text you see in them.
 */
-var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderName = '<h1 id="name" class="title-super text-thin text-uppercase">%data%</h1>';
+var HTMLheaderRole = '<span class="role">%data%</span><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
@@ -27,33 +27,33 @@ var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
 var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLskills = '<li class="flex-item skillset"><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkEmployer = '<a href="#">%data%';
 var HTMLworkTitle = ' - %data%</a>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
-var HTMLworkDescription = '<p><br>%data%</p>';
+var HTMLworkDescription = '<p><br>%data%</p><br>';
 
 var HTMLprojectStart = '<div class="project-entry"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
-var HTMLprojectDescription = '<p><br>%data%</p>';
+var HTMLprojectDescription = '<p><br>%data%</p><br>';
 var HTMLprojectImage = '<img src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
-var HTMLschoolDegree = ' -- %data%</a>';
+var HTMLschoolDegree = '<br><div class="degree-text">%data%';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
-var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+var HTMLschoolMajor = 'Major: %data%';
 
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
-var HTMLonlineSchool = ' - %data%</a>';
-var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
+var HTMLonlineClasses = '<div class="online-courses">Online Courses</div>';
+var HTMLonlineTitle = '<div class="title-text">%data%</div>';
+var HTMLonlineSchool = '<a href="#">%data%';
+var HTMLonlineDates = '<div class="date-text">%data%</div><br>';
+var HTMLonlineURL = '<a href="#">%data%</a>';
 
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
@@ -107,7 +107,7 @@ function initializeMap() {
   var locations;
 
   var mapOptions = {
-    disableDefaultUI: true
+    disableDefaultUI: false
   };
 
   /*
@@ -239,11 +239,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,231 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var formattedName = HTMLheaderName.replace("%data%", "Drew Hanratty");
+$("#header").append(formattedName);
+
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+$("#header").append(formattedRole);
+
+var bio = {
+  "name": "Drew Hanratty",
+  "role": "Web Developer",
+  "contacts":
+    {
+      "mobile": "971-337-8690",
+      "email": "drew.hanratty@gmail.com",
+      "github": "andrewhanratty",
+      "location": "Portland, OR"
+    },
+    "welcomeMessage": "Keep it creative.",
+    "biopic": "images/underwater.jpg",
+    "skills": ["HTML", "CSS", "JavaScript", "Microsoft Office", "Strong writing and presentation skills"]
+};
+
+var work = {
+  "jobs": [
+    {
+      "employer": "PELA",
+      "title": "Examiner Trainer",
+      "location": "Portland, OR",
+      "dates": "August 2016 to Present",
+      "description": "Delivered training and recertification sessions for new and existing examiners, respectively. Monitored examiner performance and provided both face-to-face and written feedback."
+    },
+    {
+      "employer": "British Council",
+      "title": "Team Leader/Examiner Trainer",
+      "location": "Beijing, China",
+      "dates": "July 2009 to June 2016",
+      "description": "Led a team of IELTS examiners, ensuring consistent performance to British Council standards through annual and semi-annual performance reviews. Delivered procedural workshops, conducted disciplinary meetings and took part in recruitment activities, including shortlisting and interviewing candidates."
+    },
+    {
+      "employer": "Ying Han Da Publishing",
+      "title": "Editor/Abridger",
+      "location": "Beijing, China",
+      "dates": "August 2001 to August 2009",
+      "description": "Abridged classic novels as part of an extended book series, published nationwide, for English-language learners. This involved rewriting the classics within certain grammatical and lexical parameters."
+    },
+    {
+      "employer": "Delter Business Institute",
+      "title": "ESL Teacher",
+      "location": "Beijing, China",
+      "dates": "August 2001 to July 2003",
+      "description": "Taught General English classes to adult learners, and acted as Materials Coordinator."
+    },
+    {
+      "employer": "AHRC-NYC",
+      "title": "Residential Services Wait List Coordinator",
+      "location": "New York City, NY",
+      "dates": "April 1997 to April 2001",
+      "description": "Headed the Residential Placement Wait List Department and facilitated the completion of the 3-year New York Cares initiative by determining the number and type of residences for which the agency should bid, according to the housing needs of those on the waiting list. Recruited and trained the Residential Placement Wait List team. Delivered community presentations."
+    },
+    {
+      "employer": "Arc of the Bluegrass",
+      "title": "Group Home Manager",
+      "location": "Lexington, KY",
+      "dates": "August 1995 to March 1997",
+      "description": "Managed group homes for the developmentally disabled. Recruited and trained residence staff, maintained financial records and supervised medication administration and direct care."
+    }
+  ]
+};
+
+var projects = {
+	"projects": [{
+		"title": "Nezha",
+		"dates": "June 2015 to July 2015",
+		"description": "Composed the score for this feature film by director Li Xiaofeng."
+	}, {
+		"title": "Romance Out of the Blue",
+		"dates": "May 2015 to June 2015",
+		"description": "Composed the score for this romantic comedy by director Ning Ying."
+	}, {
+		"title": "Portrait of a Migrant",
+		"dates": "September 2014 to Octorber 2014",
+		"description": "Composed the score for this hour-length for-TV documentary by director Fei Fan."
+	}]
+};
+
+var education = {
+	"schools": [{
+		"name": "University of Kentucky",
+		"location": "Lexington Ky",
+		"degree": "BA (summa cum laude)",
+		"major": "Psychology",
+		"dates": "August 1991 to August 1995"
+	}],
+	"onlineCourse": [{
+		"title": "Front-end Web Developer",
+		"school": "Udacity",
+		"dates": "January 2017 to May 2017",
+    "url": "https://www.udacity.com"
+	}]
+};
+
+$('#topContacts').insertAfter('hr');
+
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+$("#topContacts").append(formattedMobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+$("#topContacts").append(formattedEmail);
+var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.github);
+$("#topContacts").append(formattedGitHub);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+$("#topContacts").append(formattedLocation);
+
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+$("#footerContacts").append(formattedMobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+$("#footerContacts").append(formattedEmail);
+var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.github);
+$("#footerContacts").append(formattedGitHub);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+$("#footerContacts").append(formattedLocation);
+
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
+$("#header").append(formattedBioPic);
+
+if (bio.skills.length > 0) {
+  $("#header").append(HTMLskillsStart);
+
+  var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+  $("#skills").append(formattedSkill);
+  formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+  $("#skills").append(formattedSkill);
+  formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+  $("#skills").append(formattedSkill);
+  formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+  $("#skills").append(formattedSkill);
+};
+
+work.display = function() {
+  work.jobs.forEach(function(job) {
+    $("#workExperience").append(HTMLworkStart);
+
+    var formattedEmployer = HTMLworkEmployer.replace("%data%", job.employer);
+    var formattedTitle = HTMLworkTitle.replace("%data%", job.title);
+    var formattedEmployerTitle = formattedEmployer + formattedTitle;
+    $(".work-entry:last").append(formattedEmployerTitle);
+
+    var formattedWorkDates = HTMLworkDates.replace("%data%", job.dates);
+    $(".work-entry:last").append(formattedWorkDates);
+
+    var formattedWorkDescription = HTMLworkDescription.replace("%data%", job.description);
+    $(".work-entry:last").append(formattedWorkDescription);
+
+    var formattedWorkLocation = HTMLworkLocation.replace("%data%", job.location);
+    $(".work-entry:last").prepend(formattedWorkLocation);
+  })
+}
+
+work.display();
+
+projects.display = function() {
+    projects.projects.forEach(function(project) {
+      $("#projects").append(HTMLprojectStart);
+
+      var formattedTitle = HTMLprojectTitle.replace("%data%", project.title);
+      $(".project-entry:last").append(formattedTitle);
+
+      var formattedDates = HTMLprojectDates.replace("%data%", project.dates);
+      $(".project-entry:last").append(formattedDates);
+
+      var formattedDescription = HTMLprojectDescription.replace("%data%", project.description);
+      $(".project-entry:last").append(formattedDescription);
+  })
+}
+
+projects.display();
+
+education.display = function() {
+    for (school = 0; school < education.schools.length; school++) {
+      $("#education").append(HTMLschoolStart);
+
+      var formattedName = HTMLschoolName.replace("%data%", education.schools[school].name);
+      $(".education-entry:last").append(formattedName);
+
+      var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+      $(".education-entry:last").append(formattedDates);
+
+      var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+      $(".education-entry:last").append(formattedLocation);
+
+      var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+      $(".education-entry:last").append(formattedDegree);
+
+      var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
+      $(".education-entry:last").append(formattedMajor);
+    }
+
+      $(".education-entry:last").append(HTMLonlineClasses);
+
+    for (i = 0; i < education.onlineCourse.length; i++) {
+
+        var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourse[i].school);
+        $(".education-entry:last").append(formattedOnlineSchool);
+
+        var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourse[i].dates);
+        $(".education-entry:last").append(formattedOnlineDates);
+
+        var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourse[i].title);
+        $(".education-entry:last").append(formattedOnlineTitle);
+
+        var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourse[i].url);
+        $(".education-entry:last").append(formattedOnlineURL);
+    }
+}
+
+education.display();
+
+
+
+$(document).click(function(loc) {
+  var x = loc.pageX;
+  var y = loc.pageY;
+
+  logClicks(x, y);
+})
+
+$("#main").append(internationalizeButton);
+function inName(string) {
+  var internationalName = string.split(" ");
+  return internationalName[0] + " " + internationalName[1].toUpperCase();
+}
+
+$("#mapDiv").append(googleMap);
